c387f61e80e2c162f9b45c64a06a776f8936a0ae
Daisy <ramired2@oregonstate.edu>
2022-02-25 16:53:51 -0800

    added link create for ea indiv family tree

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -24,16 +24,27 @@ const Works = (userId) => {
       window.location.href="http://localhost:3005/creator=" + saved + "/works"
     }
   }
+
+  const createIndivTreeLinks = (treeId) => {
+    // would call getFamilyTrees(userId) (get all family tree ids for this specific user)
+    // parse through them to get the id/description
+
+    return "http://localhost:3005/treeId=" + treeId + "/create"
+    
+  }
+
+  const handleNewTree = () => {
+    // would create a new empty tree for user --> implement after family table set
+  }
   
   return(
     <div className="content">
       <h1 className="subtopic text">Here are your works!</h1>
-      <p className="description text">Lorem ipsum dolor sit amet, ea detracto legendos imperdiet mel, ius ne noster graecis minimum. Ex usu animal officiis, nam unum mediocritatem ei. Aliquip definiebas nec et, ius te mutat bonorum, at eos ferri veritus adversarium. Cu quo alterum scribentur, fastidii detracto adolescens his ne. Id eum idque moderatius deterruisset.
-                                              Audire deleniti voluptatibus no mea, ea discere perpetua iracundia vis. Natum salutandi ut quo. Pertinax principes disputationi an quo, eam vulputate adipiscing ei, justo facilis omnesque eos id. Cu nominati molestiae efficiendi usu. Ea duo habeo feugiat adipisci.
-                                              Causae persecuti eum et. Ne scribentur delicatissimi eam, cu pri vero laboramus voluptaria. Bonorum argumentum eum cu, tantas deterruisset vim ea. Duo quod decore pericula et, ea his primis eruditi. At mazim albucius eligendi eos.
-                                              Ea pro quidam tractatos, per iudico laoreet no, lucilius tacimates et usu. Duo ne amet senserit, has at illud voluptaria, nibh probatus ut vim. Tota idque nullam ne has, in error eloquentiam nec, usu ferri pertinacia efficiendi cu. Cetero alterum liberavisse vim an, accumsan qualisque constituam ad eos, his et erant graeci. Eos ea quando partem nominavi.
-                                              Sea cibo suscipiantur te. Eu atqui clita mei. Cum ut dolorum abhorreant. Nec inani menandri ex, habeo menandri nec ut.
-      </p>
+      <button className="add_tree_btn">Create New Tree</button>
+      <p className="description text"><a href= { createIndivTreeLinks(5) }>indiv family tree</a></p>
+      <p className="description text"><a href= { createIndivTreeLinks(10) }>indiv family tree</a></p>
+      <p className="description text"><a href= { createIndivTreeLinks(15) }>indiv family tree</a></p>
+      <p className="description text"><a href= { createIndivTreeLinks(20) }>indiv family tree</a></p>
     </div>
   );
 }
