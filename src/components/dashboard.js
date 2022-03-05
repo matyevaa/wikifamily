29dcc99fd118f6a0d1504d9d604d1eea13bb7b9d
Daisy <ramired2@oregonstate.edu>
2022-03-05 14:05:56 -0800

    add id/name/email to localstorage, redirect to /works

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,20 +4,13 @@ import { useUserContext } from "../context/userContext";
 const Dashboard = () => {
   const { user, logoutUser } = useUserContext();
 
-  //save the user info in localStorage for now
-  // const setUserInfoLocal = () => {
     localStorage.setItem("userEmail", JSON.stringify(user.email))
+    localStorage.setItem("userName", JSON.stringify(user.displayName))
     localStorage.setItem("userId", JSON.stringify(user.displayName))
-  // }
+    localStorage.setItem("loginVersion", JSON.stringify("firebase"))
 
-  return (
-    <div>
-      <h1>Dashboard </h1>
-      <h2>Name : {user.displayName}</h2>
-      <h2>Email : {user.email}</h2>
-      <button onClick={logoutUser}>Log out</button>
-    </div>
-  );
-};
+    window.location.href="http://localhost:3005/creator=" + user.displayName + "/works"
+}
+
 
 export default Dashboard;
