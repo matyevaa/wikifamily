f096d25b863145f6e75004e4b80ea3e1a38c85e7
Daisy <ramired2@oregonstate.edu>
2022-03-05 19:22:15 -0800

    api get tree name, output to screen

--- src/pages/IndivTree.js
+++ src/pages/IndivTree.js
@@ -1,15 +1,29 @@
 import React, {useState, useEffect} from 'react';
-
+import axios from 'axios';
 
 const IndivTree = (treeId) => {
-//   const [treeIdT, settreeIdT] = useState("");
+  const [dataDB, setData] = useState(["no data"]);
+  const [treeIdentif, setItendif] = useState();
 
   useEffect(() => {
+    // setItendif(treeId.location['pathname'].slice(8,(treeId.location['pathname']).length - 7));
+    console.log("get name for tree: ")
+    console.log(treeId.location['pathname'].slice(8,(treeId.location['pathname']).length - 7))
+    getFamilyName(treeId.location['pathname'].slice(8,(treeId.location['pathname']).length - 7));
+    
   }, []);
 
-  const getFamilyTrees = () => {
-    // Would call current /create but add in the tree id to get info
-  }
+  const getFamilyName = async(treeID) => {
+    console.log("asxdcfvgb")
+    const result = await axios (`http://localhost:5000/api1/getTreeName/${treeID}`, {
+      headers: { 'Content-Type': 'application/json'}
+    })
+    .then(result => setData(result.data[0]))
+    .catch(err => console.log(err));
+    console.log("datadb")
+    console.log(dataDB);
+    console.log(result);
+};
 
   const getTreeId = () => {
     return treeId.location['pathname'].slice(8,(treeId.location['pathname']).length - 7)
@@ -18,7 +32,7 @@ const IndivTree = (treeId) => {
   
   return(
     <div className="content">
-      <h1 className="subtopic text">Family tree ID: {getTreeId()}</h1>
+      <h1 className="subtopic text">Family tree ID: {dataDB['family_name']}</h1>
       <p className="description text">Output individuals in tree here (copy/paste from curr /create)</p>
     </div>
   );
