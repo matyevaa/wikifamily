57d0451272de6363ae93d3c51f1d269b0ae1e4d6
Alima Matyeva <matyevaa@oregonstate.edu>
2022-05-06 13:02:07 -0700

    about page content update

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,12 +7,22 @@ const About = ({
   <div className="centerDiv">
     <div className="content">
       <h1 className="subtopic text">About Page</h1>
-      <p className="description text">Lorem ipsum dolor sit amet, ea detracto legendos imperdiet mel, ius ne noster graecis minimum. Ex usu animal officiis, nam unum mediocritatem ei. Aliquip definiebas nec et, ius te mutat bonorum, at eos ferri veritus adversarium. Cu quo alterum scribentur, fastidii detracto adolescens his ne. Id eum idque moderatius deterruisset.
-                                              Audire deleniti voluptatibus no mea, ea discere perpetua iracundia vis. Natum salutandi ut quo. Pertinax principes disputationi an quo, eam vulputate adipiscing ei, justo facilis omnesque eos id. Cu nominati molestiae efficiendi usu. Ea duo habeo feugiat adipisci.
-                                              Causae persecuti eum et. Ne scribentur delicatissimi eam, cu pri vero laboramus voluptaria. Bonorum argumentum eum cu, tantas deterruisset vim ea. Duo quod decore pericula et, ea his primis eruditi. At mazim albucius eligendi eos.
-                                              Ea pro quidam tractatos, per iudico laoreet no, lucilius tacimates et usu. Duo ne amet senserit, has at illud voluptaria, nibh probatus ut vim. Tota idque nullam ne has, in error eloquentiam nec, usu ferri pertinacia efficiendi cu. Cetero alterum liberavisse vim an, accumsan qualisque constituam ad eos, his et erant graeci. Eos ea quando partem nominavi.
-                                              Sea cibo suscipiantur te. Eu atqui clita mei. Cum ut dolorum abhorreant. Nec inani menandri ex, habeo menandri nec ut.
-      </p>
+      <div className="description text">
+        <p>WikiFamily is the product of a 2021-2022 capstone project at <a href="https://oregonstate.edu/">Oregon State University</a>.
+        <br/><br/>The developing team includes:
+          <ul>
+            <li className="name">Alex Uong</li>
+            <li className="name">Alima Matyeva</li>
+            <li className="name">Daisy Ramirez</li>
+          </ul>
+        </p>
+        <p>WikiFamily was created as an online platform to help counteract some issues noted from other family genealogy software which include downloading additional software, limited sharing functionalities, and a different representation of family trees for easy reading.
+        <br/><br/>WikiFamily aims to be a user-friendly website that helps users understand their individual genealogy and provide an easy to read and encode family history for posterity.</p>
+        <h2 id="acknowledgements" className="subtopic text">Acknowledgments</h2>
+        <p>The WikiFamily developer team would like to acknowledge and thank <span className="name">Dr. Joseph Louis</span> for bringing this project idea to our attention and allowing us to contribute to his ideal family tree app.
+          <br/><br/>The Wikifamily team would also like to thank professor <span className="name">Rob Hess</span> for guiding us in creating a tree view algorithm and our TA <span className="name">Matt Olson</span> for navigating us and tracking our success.
+        </p>
+      </div>
     </div>
   </div>
   );
