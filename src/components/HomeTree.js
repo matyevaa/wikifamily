cb32aa344bb80449a0dd0b45598144e20c424247
alexuong <56491452+alexuong@users.noreply.github.com>
2022-05-20 17:59:57 -0700

    Fixed add/edit child/spouse menu styling

--- src/components/HomeTree.js
+++ src/components/HomeTree.js
@@ -705,7 +705,7 @@ const HomeTree = (props) => {
               <div className="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                 <label
                   htmlFor="FirstName"
-                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs text-base font-medium text-gray-900"
                 >
                   First Name
                 </label>
@@ -714,7 +714,7 @@ const HomeTree = (props) => {
                   name="firstName"
                   {...register("firstName")}
                   required
-                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                  className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Jane"
                   defaultValue={selectedData && editOrAdd == 4? selectedData.label.props.name : ""}
                 />
@@ -724,7 +724,7 @@ const HomeTree = (props) => {
               <div className="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                 <label
                   htmlFor="lastName"
-                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs text-base font-medium text-gray-900"
                 >
                   Last Name
                 </label>
@@ -733,7 +733,7 @@ const HomeTree = (props) => {
                   name="lastName"
                   {...register("lastName")}
                   required
-                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                  className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Doe"
                   defaultValue={selectedData && editOrAdd == 4 ? selectedData.label.props.last : ""}
                 />
@@ -744,7 +744,7 @@ const HomeTree = (props) => {
               <div className="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                 <label
                   htmlFor="gender"
-                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs text-base font-medium text-gray-900"
                 >
                   Gender
                 </label>
@@ -753,7 +753,7 @@ const HomeTree = (props) => {
                   name="gender"
                   {...register("gender")}
                   required
-                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                  className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Male"
                   defaultValue={selectedData && editOrAdd == 4 ? selectedData.label.props.gender : ""}
                 />
@@ -764,7 +764,7 @@ const HomeTree = (props) => {
               <div className="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                 <label
                   htmlFor="dateOfBirth"
-                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs text-base font-medium text-gray-900"
                 >
                   Date of Birth
                 </label>
@@ -773,7 +773,7 @@ const HomeTree = (props) => {
                   required
                   type="date"
                   name="dateOfBirth"
-                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                  className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Doe"
                   defaultValue={selectedData && editOrAdd == 4 ? selectedData.label.props.dob : ""}
                 />
@@ -783,7 +783,7 @@ const HomeTree = (props) => {
               <div className="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                 <label
                   htmlFor="dateOfDeath"
-                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+                  className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs text-base font-medium text-gray-900"
                 >
                   Date of Death
                 </label>
@@ -792,7 +792,7 @@ const HomeTree = (props) => {
                   required
                   type="date"
                   name="dateOfDeath"
-                  className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                  className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Doe"
                   defaultValue={!selectedData && editOrAdd == 4 ? selectedData.label.props.dod : ""}
                 />
